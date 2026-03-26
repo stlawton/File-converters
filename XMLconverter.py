0357d7fd3c92e41c0cc2c13dc8f56b565693ccc2
stlawton <lawtons347@gmail.com>
2026-03-26 20:29:21 +0000

    Fixed some errors in the code

--- XMLconverter.py
+++ XMLconverter.py
@@ -12,30 +12,31 @@ def convert_xml_to_json(xml_file):
         for child in root:
 
             tweet = {
-                "Text": child.findtext("text"),
-                "ID": int(child.findtext("ID", 0),
+                "Text": child.findtext("Text", ""),
+                "ID": int(child.findtext("ID", 0)),
                 "QuotedID": int(child.findtext("QuotedID", 0)),
-                "RetweetedText": child.findtext("RetweetedText"),
-                "RetweetedHandle": child.findtext("RetweetedHandle"),
-                "RetweetedName": child.findtext("RetweetedName"),
+                "RetweetedText": child.findtext("RetweetedText", ""),
+                "RetweetedHandle": child.findtext("RetweetedHandle", ""),
+                "RetweetedName": child.findtext("RetweetedName", ""),
                 "Retweets": int(child.findtext("Retweets", 0)),
-                "Source": child.findtext("Source"),
-                "UserHandle": child.findtext("UserHandle"),
-                "UserName": child.findtext("UserName")
+                "Source": child.findtext("Source", ""),
+                "UserHandle": child.findtext("UserHandle", ""),
+                "UserName": child.findtext("UserName", "")
             }
             tweets.append(tweet)
         
         output = {"Tweets": tweets}
         json_out = json.dumps(output, ensure_ascii=False, separators=(',', ':'))
         json_out = json_out.replace("<", "\\u003c").replace(">", "\\u003e")
+        json_out = json_out.replace("&amp;", "\\u0026amp;")
         print(json_out)
         
     except FileNotFoundError:
-        print(f"Error: File {csv_file} not found.", file=sys.stderr)
+        print(f"Error: File {xml_file} not found.", file=sys.stderr)
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        print("Error: Please provide a CSV file.")
+        print("Error: Please provide an XML file.")
         sys.exit(1)
 
-    convert_csv_to_json(sys.argv[1])
+    convert_xml_to_json(sys.argv[1])
